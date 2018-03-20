6b1a7918fc9d915ab6b475b33cc60667abbce945
Kushal Gupta <kushalguptadeveloper@gmail.com>
2018-03-21 01:17:16 +0530

    completed

--- app/src/main/java/com/teamrevelador/hack40_android/Activities/FeedbackPage1.java
+++ app/src/main/java/com/teamrevelador/hack40_android/Activities/FeedbackPage1.java
@@ -8,19 +8,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.teamrevelador.hack40_android.R;
+import com.teamrevelador.hack40_android.Retrofit.ApiClient;
+import com.teamrevelador.hack40_android.Retrofit.Responses.SignUpResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by kushalgupta on 19/03/18.
  */
 
 public class FeedbackPage1 extends AppCompatActivity {
-
+    EditText homeTown, monumentName, feedback01, monumentName2, feedback02, monumentName3, feedback03, monumentName4, feedback04, monumentName5, feedback05;
+    RatingBar r1, r2, r3, r4, r5;
+    Button btn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,7 +38,7 @@ public class FeedbackPage1 extends AppCompatActivity {
 
 
         Spinner sp1 = findViewById(R.id.spinner1);
-
+        btn = findViewById(R.id.button);
 
         if ("car" == String.valueOf(sp1.getSelectedItem())) {
             Toast.makeText(getApplicationContext(), "car", Toast.LENGTH_SHORT).show();
@@ -51,17 +60,132 @@ public class FeedbackPage1 extends AppCompatActivity {
 //        rl.addView(v2,lp);
 //    }
 //});
+        homeTown = findViewById(R.id.editText);
+        String home = homeTown.getText().toString();
+        final String transitVia = String.valueOf(sp1.getSelectedItem());
+        monumentName = findViewById(R.id.et_place_visited);
+        final String placeVisited1 = monumentName.getText().toString();
+
+        r1 = findViewById(R.id.ratingBar);
+        final String rating1 = String.valueOf(r1.getNumStars());
+        feedback01 = findViewById(R.id.et_feedback1);
+        String Feedback1 = feedback01.getText().toString();
+
+        monumentName2 = findViewById(R.id.et_place_visited2);
+        final String placeVisited2 = monumentName2.getText().toString();
+
+        r2 = findViewById(R.id.ratingBar2);
+        final String rating2 = String.valueOf(r2.getNumStars());
+        feedback02 = findViewById(R.id.et_feedback2);
+        String Feedback2 = feedback02.getText().toString();
+
+        monumentName3 = findViewById(R.id.et_place_visited3);
+        final String placeVisited3 = monumentName3.getText().toString();
+
+        r3 = findViewById(R.id.ratingBar3);
+        final String rating3 = String.valueOf(r3.getNumStars());
+        feedback03 = findViewById(R.id.et_feedback3);
+        String Feedback3 = feedback03.getText().toString();
+
+        monumentName4 = findViewById(R.id.et_place_visited4);
+        final String placeVisited4 = monumentName4.getText().toString();
+
+        r4 = findViewById(R.id.ratingBar4);
+        final String rating4 = String.valueOf(r4.getNumStars());
+        feedback04 = findViewById(R.id.et_feedback4);
+        String Feedback4 = feedback04.getText().toString();
 
 
+        monumentName5 = findViewById(R.id.et_place_visited5);
+        final String placeVisited5 = monumentName5.getText().toString();
+
+        r5 = findViewById(R.id.ratingBar5);
+        final String rating5 = String.valueOf(r5.getNumStars());
+        feedback05 = findViewById(R.id.et_feedback5);
+        String Feedback5 = feedback05.getText().toString();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ApiClient apiClient1 = new ApiClient();
+                String FEEDBACK = String.valueOf(1);
+                apiClient1.getApiInterface().postFeedback(FEEDBACK, "6", placeVisited1, feedback01.getText().toString(), transitVia, rating1)
+                        .enqueue(new Callback<SignUpResponse>() {
+                            @Override
+                            public void onResponse(Call<SignUpResponse> call, Response<SignUpResponse> response) {
+                                Toast.makeText(getApplicationContext(), "FEEDBACK SUBMITTED, THANK YOU", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onFailure(Call<SignUpResponse> call, Throwable t) {
+
+                            }
 
 
+                        });
+                ApiClient apiClient2 = new ApiClient();
+                String FEEDBACK2 = String.valueOf(1);
+                apiClient1.getApiInterface().postFeedback(FEEDBACK, "6", placeVisited2, feedback02.getText().toString(), transitVia, rating2)
+                        .enqueue(new Callback<SignUpResponse>() {
+                            @Override
+                            public void onResponse(Call<SignUpResponse> call, Response<SignUpResponse> response) {
+                                Toast.makeText(getApplicationContext(), "FEEDBACK SUBMITTED, THANK YOU", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onFailure(Call<SignUpResponse> call, Throwable t) {
+
+                            }
+
+        });
+
+                ApiClient apiClient3 = new ApiClient();
+                String FEEDBACK3 = String.valueOf(1);
+                apiClient1.getApiInterface().postFeedback(FEEDBACK, "6", placeVisited3, feedback03.getText().toString(), transitVia, rating3)
+                        .enqueue(new Callback<SignUpResponse>() {
+                            @Override
+                            public void onResponse(Call<SignUpResponse> call, Response<SignUpResponse> response) {
+                                Toast.makeText(getApplicationContext(), "FEEDBACK SUBMITTED, THANK YOU", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onFailure(Call<SignUpResponse> call, Throwable t) {
+
+                            }});
+                ApiClient apiClient4 = new ApiClient();
+                String FEEDBACK4 = String.valueOf(1);
+                apiClient1.getApiInterface().postFeedback(FEEDBACK, "6", placeVisited4, feedback04.getText().toString(), transitVia, rating4)
+                        .enqueue(new Callback<SignUpResponse>() {
+                            @Override
+                            public void onResponse(Call<SignUpResponse> call, Response<SignUpResponse> response) {
+                                Toast.makeText(getApplicationContext(), "FEEDBACK SUBMITTED, THANK YOU", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onFailure(Call<SignUpResponse> call, Throwable t) {
+
+                            }});
+                ApiClient apiClient5 = new ApiClient();
+
+                apiClient1.getApiInterface().postFeedback(FEEDBACK, "6", placeVisited5, feedback05.getText().toString(), transitVia, rating5)
+                        .enqueue(new Callback<SignUpResponse>() {
+                            @Override
+                            public void onResponse(Call<SignUpResponse> call, Response<SignUpResponse> response) {
+                                Toast.makeText(getApplicationContext(), "FEEDBACK SUBMITTED, THANK YOU", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onFailure(Call<SignUpResponse> call, Throwable t) {
+
+                            }});
 //        LayoutInflater layoutInflater=getLayoutInflater();
 //        View v=layoutInflater.inflate(R.layout.feedback_dynamic_rating,null);
 //        ViewGroup main= findViewById(R.id.scroll_dynamic_container);
 //        main.addView(v,0);
 
 
-
-
-    }}
+    }
+});
+    }
+}
 
--- app/src/main/java/com/teamrevelador/hack40_android/Activities/MonumentDescription.java
+++ app/src/main/java/com/teamrevelador/hack40_android/Activities/MonumentDescription.java
@@ -1,5 +1,7 @@
 package com.teamrevelador.hack40_android.Activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
@@ -12,7 +14,10 @@ import android.widget.RatingBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.teamrevelador.hack40_android.R;
+
+import java.util.Locale;
 
 /**
  * Created by kushalgupta on 20/03/18.
@@ -20,20 +25,20 @@ import com.teamrevelador.hack40_android.R;
 
 public class MonumentDescription extends AppCompatActivity {
     ImageView imageView;
-    TextView tvMonumentName,description,facte1,facte2,facte3,facte4,facte5,facte6,facte7,facte8,facte9,facte10;
+    TextView tvMonumentName, Description, facte1, facte2, facte3, facte4, facte5, facte6, facte7, facte8, facte9, facte10;
     RatingBar monumentRating;
     ImageButton directionButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    setContentView(R.layout.monument_description);
-    imageView=findViewById(R.id.iv_monument_pic);
-    tvMonumentName = findViewById(R.id.textView6);
-    monumentRating = findViewById(R.id.ratingBar);
-    directionButton = findViewById(R.id.directions_rating_btn);
-    description = findViewById(R.id.description);
-    facte1 = findViewById(R.id.fact1);
+        setContentView(R.layout.monument_description);
+        imageView = findViewById(R.id.iv_monument_pic);
+        tvMonumentName = findViewById(R.id.textView6);
+        monumentRating = findViewById(R.id.ratingBar);
+        directionButton = findViewById(R.id.directions_rating_btn);
+        Description = findViewById(R.id.description);
+        facte1 = findViewById(R.id.fact1);
         facte2 = findViewById(R.id.fact2);
         facte3 = findViewById(R.id.fact3);
         facte4 = findViewById(R.id.fact4);
@@ -46,24 +51,47 @@ public class MonumentDescription extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         String monumentName = bundle.getString("monumentName");
-         String latitude=bundle.getString("latitude");
-         String longitude=bundle.getString("longitude");
-         String description=bundle.getString("description");
-         String photo=bundle.getString("photoUrl");
-         String rating=bundle.getString("rating");
-         String visits=bundle.getString("visits");
-         String fact1=bundle.getString("fact1");
-         String fact2=bundle.getString("fact2");
-         String fact3=bundle.getString("fact3");
-         String fact4=bundle.getString("fact4");
-         String fact5=bundle.getString("fact5");
-         String fact6=bundle.getString("fact6");
-         String fact7=bundle.getString("fact7");
-         String fact8=bundle.getString("fact8");
-         String fact9=bundle.getString("fact9");
-         String fact10=bundle.getString("fact10");
+        final String latitude = bundle.getString("latitude");
+        final String longitude = bundle.getString("longitude");
+        String description = bundle.getString("description");
+        String photo = bundle.getString("photoUrl");
+        String rating = bundle.getString("rating");
+        String visits = bundle.getString("visits");
+        String fact1 = bundle.getString("fact1");
+        String fact2 = bundle.getString("fact2");
+        String fact3 = bundle.getString("fact3");
+        String fact4 = bundle.getString("fact4");
+        String fact5 = bundle.getString("fact5");
+        String fact6 = bundle.getString("fact6");
+        String fact7 = bundle.getString("fact7");
+        String fact8 = bundle.getString("fact8");
+        String fact9 = bundle.getString("fact9");
+        String fact10 = bundle.getString("fact10");
 
+Description.setText(description);
+tvMonumentName.setText(monumentName);
+        Glide.with(getApplicationContext()).load(photo).into(imageView);
+        monumentRating.setNumStars(Integer.parseInt(rating));
+        facte1.setText(fact1);
+        facte2.setText(fact2);
+        facte3.setText(fact3);
+        facte4.setText(fact4);
+        facte5.setText(fact5);
+        facte6.setText(fact6);
+        facte7.setText(fact7);
+        facte8.setText(fact8);
+        facte9.setText(fact9);
+        facte10.setText(fact10);
 
+        directionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uri = String.format(Locale.ENGLISH, "geo:%f,%f", latitude, longitude);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                startActivity(intent);
+
+            }
+        });
 
 
     }
